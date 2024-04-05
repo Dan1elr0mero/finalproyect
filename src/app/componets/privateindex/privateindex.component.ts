a66a2ac162217bced27fc85e5861dc78d1d2b563
Daniel Romero <romeroblessjunior@gmail.com>
2024-04-05 15:42:10 -0500

    bassbox

--- src/app/componets/privateindex/privateindex.component.ts
+++ src/app/componets/privateindex/privateindex.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-privateindex',
   standalone: true,
   imports: [],
   templateUrl: './privateindex.component.html',
-  styleUrl: './privateindex.component.css'
+  styleUrl: './privateindex.component.css',
 })
 export class PrivateindexComponent {
+  // products: Product[] = []
 
+  constructor(private productService: ProductsService) {}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: (value) => {
+        // this.products = value
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
